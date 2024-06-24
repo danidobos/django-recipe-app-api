cde177092cda4af7ca3f34ab76e0b78ba202a04b
danidobos <danidobos97@gmail.com>
2024-06-24 11:41:57 +0200

    Implement create user API

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -46,7 +46,7 @@ class PublicUserApiTest(TestCase):
             'name': 'Test Name',
         }
 
-        create_user(payload)
+        create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
