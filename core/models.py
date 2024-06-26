d52a22f8f6015ca933d0940757760716ce9cf949
danidobos <danidobos97@gmail.com>
2024-06-26 09:38:20 +0200

    Implement recipe model and test

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@
 Database models
 """
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -44,5 +45,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    # The field that we want to use for authentication. By default it's the username field, but we can override it, to use email instead
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
