da2075ecbd5562c7ff0fc26a23a034a1fc5b3db2
danidobos <danidobos97@gmail.com>
2024-06-28 11:02:09 +0200

    Implement tag listing API

--- recipe/urls.py
+++ recipe/urls.py
@@ -12,5 +12,6 @@ app_name = 'recipe'
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 urlpatterns = [path('', include(router.urls))]
