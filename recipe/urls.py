8cbd7e7813a629f2b522f8b0324055d2be3b8707
danidobos <danidobos97@gmail.com>
2024-07-03 10:37:28 +0200

    Implement ingredient listing API

--- recipe/urls.py
+++ recipe/urls.py
@@ -13,5 +13,6 @@ app_name = 'recipe'
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 urlpatterns = [path('', include(router.urls))]
